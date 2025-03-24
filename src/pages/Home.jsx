a56a5f50dd9fd31eb93bede48088787f7c717c93
Edward2003GHub <edwardalkopti486@gmail.com>
2025-03-24 22:38:51 +0300

    styling home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home-container">
+      <img src="https://bau.edu.jo/bauliveportal/attch/13052_1.jpg" alt="bau-photo" />
+      <div>
+        <p>list of events</p>
+        <button>view more events</button>
+      </div>
+
+      <div>
+        <h2>Organizations</h2>
+        <p>list of organizations</p>
+      </div>
+
+      <div>
+        <h2>Latest News</h2>
+        <p>list of organizations</p>
+      </div>
+    </div>
+  );
 }
