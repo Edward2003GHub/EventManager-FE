62eb27384e5ebe4b8d9514e28016ae223c147f98
Edward2003GHub <edwardalkopti486@gmail.com>
2025-04-01 18:36:51 +0300

    deleted input.jsx

--- src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export default function Input({ id, label, error, ...props}) {
-  return (
-    <div className="register-input">
-      <input {...props} id={id} placeholder={label} />
-      <p>{error}</p>
-    </div>
-  );
-}
