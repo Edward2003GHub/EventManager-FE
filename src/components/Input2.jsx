34037059732cb1f7a0eb80cf38967c527a05ef2c
Edward2003GHub <edwardalkopti486@gmail.com>
2025-04-06 20:43:04 +0300

    login register design

--- src/components/Input2.jsx
+++ src/components/Input2.jsx
@@ -4,20 +4,10 @@ import TextField from "@mui/material/TextField";
 export default function Input2({ label, error, errorText, ...props }) {
   return (
     <TextField
-      sx={{
-        "& label.Mui-focused": {
-          color: "rgb(126, 126, 255)", // Change label color on focus
-        },
-        "& .MuiOutlinedInput-root": {
-          "&.Mui-focused fieldset": {
-            borderColor: "rgb(126, 126, 255)", // Change border color on focus
-          },
-        },
-      }}
       error={error}
       label={label}
-      {...props}
       helperText={error && errorText}
+      {...props}
     />
   );
 }
